6fa28a86befae999eeec77804324cb7c0bcc3da6
Egor Stepanov <stepanovep@gmail.com>
2020-10-27 12:43:25 +0300

    catch mongo duplicate exception

--- src/main/java/stepanovep/fut21/mongo/AuctionService.java
+++ src/main/java/stepanovep/fut21/mongo/AuctionService.java
@@ -1,6 +1,9 @@
 package stepanovep.fut21.mongo;
 
+import com.mongodb.MongoWriteException;
 import com.mongodb.client.MongoCollection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,8 @@ import static java.util.Objects.requireNonNull;
 @Service
 public class AuctionService {
 
+    private static final Logger log = LoggerFactory.getLogger(AuctionService.class);
+
     @Autowired
     private MongoCollection<ActiveAuction> activeAuctions;
 
@@ -21,12 +26,20 @@ public class AuctionService {
 
     public void insertActiveAuction(@Nonnull ActiveAuction activeAuction) {
         requireNonNull(activeAuction, "activeAuction");
-        activeAuctions.insertOne(activeAuction);
+        try {
+            activeAuctions.insertOne(activeAuction);
+        } catch (MongoWriteException exc) {
+            log.error("Cannot insert active auction: ", exc);
+        }
     }
 
     public void insertWonAuction(@Nonnull WonAuction wonAuction) {
         requireNonNull(wonAuction, "wonAuction");
-        wonAuctions.insertOne(wonAuction);
+        try {
+            wonAuctions.insertOne(wonAuction);
+        } catch (MongoWriteException exc) {
+            log.error("Cannot insert won auction: ", exc);
+        }
     }
 
     public Optional<ActiveAuction> getActiveAuction(@Nonnull String tradeId) {
